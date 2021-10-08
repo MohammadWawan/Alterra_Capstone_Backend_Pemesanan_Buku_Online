4ac143f686b51fb09e6e89b6da68b3b668b4f598
Mohammad Wawan <40724858+sompretofimalahua@users.noreply.github.com>
2021-10-08 12:33:55 +0700

    push ci cd

--- mvc/models/karyawans/karyawan_login.go
+++
@@ -1,6 +0,0 @@
-package karyawans
-
-type KaryawanLogin struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
--- mvc/models/karyawans/karyawan_register.go
+++
@@ -1,7 +0,0 @@
-package karyawans
-
-type KaryawanRegister struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
